e4ee5ed5cabf84f24cacc4cac817c83395a716b0
Elizabeth Eymann <elizabeth.eymann@gmail.com>
2020-10-14 08:20:17 -0700

    add stopwatch into the scoreboard.

---
+++ src/components/Stopwatch.js
@@ -0,0 +1,17 @@
+//This is a stateful component, so we have to import component from react
+import React, { Component } from 'react';
+
+class Stopwatch extends Component {
+  render() {
+    return (
+      <div className="stopwatch">
+        <h2>Stopwatch</h2>
+        <span className="stopwatch-time">0</span>
+        <button>Start</button>
+        <button>Reset</button>
+        </div>
+    );
+  }
+}
+
+export default Stopwatch;
